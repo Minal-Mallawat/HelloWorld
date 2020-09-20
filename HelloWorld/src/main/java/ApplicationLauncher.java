e8c97124a38e9c8482504466b83ee751f8fa3f33
minal <minal@minal-Lenovo-ideapad-330-15IKB>
2020-09-20 16:11:07 +0530

    Base Project

---
+++ HelloWorld/src/main/java/ApplicationLauncher.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+
+public class ApplicationLauncher {
+
+public static void main(String[] args) {
+
+SpringApplication.run(HelloWorldController.class, args);
+
+}
+
+}
